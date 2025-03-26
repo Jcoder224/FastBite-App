70c1bff766616995ecd8b91c8c8fadede2b560d9
Jcoder224 <gulshan9305504804@gmail.com>
2025-03-26 14:32:52 +0530

    Update commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: process.env.HOST || 'localhost',  // Use 'localhost' for local, '0.0.0.0' for deployment
+    port: process.env.PORT || 5173,         // Default port 5173
+    strictPort: true,                        // Ensures the same port is used
+  }
 })
